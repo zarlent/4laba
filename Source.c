0adb198539949ef61cc276cd76b17a8dc7e045f2
zarlent <alvdp@mail.ru>
2025-10-08 02:17:58 +0300

    Add files via upload

---
+++ Source.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_CTYPE, "RUS");
+
+    int A, B;
+
+    printf("Введите значения датчиков A и B: ");
+    scanf("%d %d", &A, &B);
+
+    if ((A % 2) != (B % 2)) {
+        printf("Сирена включена!\n");
+    }
+    else {
+        printf("Сирена выключена.\n");
+    }
+
+    return 0;
+}
